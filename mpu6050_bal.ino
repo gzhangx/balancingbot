c563b8d28b572fc01a6b2d49ead2f70e44bab7d5
Gang Zhang <gzhangx@hotmail.com>
2020-02-12 15:57:20 -0500

    initial

--- mpu6050_bal.ino
+++ mpu6050_bal.ino
@@ -8,7 +8,8 @@
 #include "I2Cdev.h"
 #include <PID_v1.h> //From https://github.com/br3ttb/Arduino-PID-Library/blob/master/PID_v1.h
 #include "MPU6050_6Axis_MotionApps20.h" //https://github.com/jrowberg/i2cdevlib/tree/master/Arduino/MPU6050
-
+//A5 => SCL
+//A4 => SDA
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
     #include "Wire.h"
 #endif
@@ -30,32 +31,62 @@ float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gra
 
  
 
-/*********Tune these 4 values for your BOT*********/
-double setpoint= 172; //set the value when the bot is perpendicular to ground using serial monitor. 
-//Read the project documentation on circuitdigest.com to learn how to set these values
-double Kp = 21; //Set this first
-double Kd = 0.8; //Set this secound
-double Ki = 140; //Finally set this 
-/******End of values setting*********/
+double setpoint= 172;    //bigger to empty side 
+double Kp = 100;
+double Kd = 0.3; 
+double Ki = 350;
 
 double input, output;
-PID pid(&input, &output, &setpoint, Kp, Ki, Kd, DIRECT);
+/******End of values setting*********/
 
-int EN_B = 12;      //Enable pin for first motor
-int IN_B = 11;       //control pin for first motor
-int IN_A = 10;
-int EN_A = 9;
+int motorCounter = 0;
+int motorCounterMax = 255;
 
+int motorPins[][2] = {
+  {8,9},
+  {6, 7}
+};
+int motorSpeed[]={0,0};
+
+int BOUND=20;
+
+int potms[] = {A0, A1, A2};
+const int POTSNUM = 3;
+
+
+PID pid = PID(&input, &output, &setpoint, Kp, Ki, Kd, DIRECT);
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady()
+{
+    mpuInterrupt = true;
+}
+
+void serprintln(String s) {
+  if (Serial) Serial.println(s);    
+}
 void setup() {
-    pinMode(EN_B, OUTPUT);
-    pinMode(IN_B, OUTPUT);  
-    pinMode(EN_A, OUTPUT);
-    pinMode(IN_A, OUTPUT);  
-    
+       
     Serial.begin(115200);
-    while (!Serial);
+    serprintln("serial initialized");
+
+    pid.SetMode(AUTOMATIC);
+    pid.SetSampleTime(10);
+    pid.SetOutputLimits(-motorCounterMax, motorCounterMax);  
+    
+    for (int i = 0; i < POTSNUM; i++) {
+      pinMode(potms[i], INPUT);
+    }
+    
+    for (int i = 0; i < 2; i++) {
+      for(int k = 0; k < 2; k++) {
+        int cur = motorPins[i][k];
+        pinMode(cur, OUTPUT);
+      }
+    }
+    
     #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-        Serial.println("wire.begin");
+        serprintln("wire.begin");
         Wire.begin();
         Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
     #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
@@ -63,47 +94,46 @@ void setup() {
     #endif
       
   // initialize device
-    Serial.println(F("Initializing I2C devices..."));
+    serprintln(F("Initializing I2C devices..."));
     
     mpu.initialize();
     //pinMode(INTERRUPT_PIN, INPUT);
 
      // verify connection
-    Serial.println(F("Testing device connections..."));
-    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+    serprintln(F("Testing device connections..."));
+    serprintln(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
 
     // load and configure the DMP
     devStatus = mpu.dmpInitialize();
 
     
     // supply your own gyro offsets here, scaled for min sensitivity
-    mpu.setXGyroOffset(220);
-    mpu.setYGyroOffset(76);
-    mpu.setZGyroOffset(-85);
-    mpu.setZAccelOffset(1688); 
+    mpu.setXGyroOffset(82);
+    mpu.setYGyroOffset(-65);
+    mpu.setZGyroOffset(-8);
+    mpu.setXAccelOffset(402); 
+    mpu.setYAccelOffset(838); 
+    mpu.setZAccelOffset(1564); 
 
       // make sure it worked (returns 0 if so)
     if (devStatus == 0)
     {
         // turn on the DMP, now that it's ready
-        Serial.println(F("Enabling DMP..."));
+        serprintln(F("Enabling DMP..."));
         mpu.setDMPEnabled(true);
+
+        attachInterrupt(digitalPinToInterrupt(2), dmpDataReady, RISING);
 
         // enable Arduino interrupt detection
         Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
         //Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
 
         // set our DMP Ready flag so the main loop() function knows it's okay to use it
-        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        serprintln(F("DMP ready! Waiting for first interrupt..."));
         dmpReady = true;
 
         // get expected DMP packet size for later comparison
         packetSize = mpu.dmpGetFIFOPacketSize();
-        
-        //setup PID
-        pid.SetMode(AUTOMATIC);
-        pid.SetSampleTime(10);
-        pid.SetOutputLimits(-255, 255);  
     }
     else
     {
@@ -113,59 +143,99 @@ void setup() {
         // (if it's going to break, usually the code will be 1)
         Serial.print(F("DMP Initialization failed (code "));
         Serial.print(devStatus);
-        Serial.println(F(")"));
+        serprintln(F(")"));
     }
 
 }
 
-char temps1[16];
-char temps2[16];
-char temps3[16];
- char debugline[256];
 
 void loop() {
-    // if programming failed, don't try to do anything
-    if (!dmpReady) return;
+  //loop_simple();
+  //loop_motor();
+  loop_balance();
+}
+void loop_simple() {
+  int who = 1;
+   if (Serial.available()){
+    char dbgstr[128];
+    int c = Serial.read();
+    if (c == 'F') motorControlDrive(who,'F');
+    if (c == 'R') motorControlDrive(who,'R');
+    if (c == 'S') motorControlDrive(who,'S');
+ 
+    
+  }
+}
+int dbgspeed = 0;
+void loop_motor() {
+  motorCounterMax = 5;
+  motorCounter++;
+  if (motorCounter>motorCounterMax)motorCounter=0;
+  char dir = 'F';
+  if (Serial.available()){
+    char dbgstr[128];
+    int c = Serial.read();
+    if (c == 'F') dir = 'F';
+    if (c == 'R') dir = 'R';
+    if (c == 'W') dbgspeed++;
+    if (c=='D') dbgspeed--;
+    if (dbgspeed < 0) dbgspeed = 0;
+    if (dbgspeed > motorCounterMax) dbgspeed = motorCounterMax;
+    sprintf(dbgstr,"%c %i", c, dbgspeed);
+    serprintln(dbgstr);
+    int sp = dbgspeed;
+  if (dir == 'R') sp = -sp;
+  motorSpeed[0] = motorSpeed[1] = sp;
+  }
+  
+  motorControlAll();  
+}
 
-    // wait for MPU interrupt or extra packet(s) available
+void loop_balance() {
+  if (!dmpReady) return;
+  motorCounter++;
+  if (motorCounter>motorCounterMax)motorCounter=0;
+  int potVal[POTSNUM];
+  for (int i = 0; i < POTSNUM; i++) {
+    potVal[i] = analogRead(potms[i]);     
+  }
+  //Kp = potVal[0]+1;
+  //Ki = potVal[1];
+  //setpoint = map(potVal[2], 0, 1023, 180-BOUND, 180+BOUND);   
+  //int pot_a2_val =  analogRead(POT_A2); //values 786 (or 642 for batt) to 0
+       
+  motorControlAll();    
 
+  while (!mpuInterrupt && fifoCount < packetSize) 
+  {  
+     pid.Compute();           
+     motorSpeed[0] = output;
+     motorSpeed[1] = output;
         
-        //no mpu data - performing PID calculations and output to motors     
-        pid.Compute();   
-        
-        //Print the value of Input and Output on serial monitor to check how it is working.
-        //Serial.print(input); Serial.print(" =>"); Serial.println(output);
-        dtostrf(ypr[1], 4, 0, temps1);
-        sprintf(debugline, "%3i %3i %3i  %5i=>%5i", (int)(ypr[0]*100), (int)(ypr[1]*100), (int)(ypr[2]*100), (int)input, (int)output);
-        Serial.println(debugline);
-        if (input>150 && input<200){//If the Bot is falling 
-          
-        if (output>0) //Falling towards front 
-        Forward(); //Rotate the wheels forward 
-        else if (output<0) //Falling towards back
-        Reverse(); //Rotate the wheels backward 
-        }
-        else //If Bot not falling
-        Stop(); //Hold the wheels still
+     if (input<(setpoint - BOUND) || input> (setpoint+BOUND)){     
+         motorSpeed[0] = motorSpeed[1] = 0;
+     }
+     if (mpuInterrupt){
+        serprintln("int="+String(mpuInterrupt)+" i=" +String(input)+" o=" + String(output));
+     }
         
         
-   
-    mpuIntStatus = mpu.getIntStatus();
+        
+      // reset interrupt flag and get INT_STATUS byte
+      mpuInterrupt = false;
+      mpuIntStatus = mpu.getIntStatus();
 
-    // get current FIFO count
-    fifoCount = mpu.getFIFOCount();
-
-    // check for overflow (this should never happen unless our code is too inefficient)
-    if ((mpuIntStatus & 0x10) || fifoCount == 1024)
-    {
+      // get current FIFO count
+      fifoCount = mpu.getFIFOCount();
+      
+      if ((mpuIntStatus & 0x10) || fifoCount == 1024)
+      {
         // reset so we can continue cleanly
         mpu.resetFIFO();
-        Serial.println(F("FIFO overflow!"));
-
-    // otherwise, check for DMP data ready interrupt (this should happen frequently)
-    }
-    else if (mpuIntStatus & 0x02)
-    {
+        serprintln("FIFO overflow!");   
+      }
+      else if (mpuIntStatus & 0x02)
+      {
         // wait for correct available data length, should be a VERY short wait
         while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
 
@@ -179,48 +249,41 @@ void loop() {
         mpu.dmpGetQuaternion(&q, fifoBuffer); //get value for q
         mpu.dmpGetGravity(&gravity, &q); //get value for gravity
         mpu.dmpGetYawPitchRoll(ypr, &q, &gravity); //get value for ypr
-
-        input = ypr[1] * 180/M_PI + 180;
-
-
-        /*
-Serial.print("ypr\t");
-            Serial.print(ypr[0] * 180/M_PI);
-            Serial.print("\t");
-            Serial.print(ypr[1] * 180/M_PI);
-            Serial.print("\t");
-            Serial.println(ypr[2] * 180/M_PI);
-            */
+        input = ((ypr[1] * 180)/M_PI) + 180;
    }
 }
 
-void motor(char who, char op, int speed) {
-    if (speed < 0) speed = -speed;
-    speed = map(speed, 0, 255, 0, 255);
-    int IN = who == 'A'?IN_A:IN_B;
-    int EN = who == 'A'?EN_A:EN_B;
-    int lh = op == 'L'?LOW:HIGH;
-    digitalWrite(IN, lh);
-    //digitalWrite(EN_A, HIGH);
-    analogWrite(EN, speed);
-}
-void Forward() //Code to rotate the wheel forward 
-{
-    motor('A', 'H', output);
-    motor('B', 'H', output);
-    Serial.print("F"); //Debugging information 
+void motorControlAll() {
+  for (int i = 0; i < 2; i++)
+    motorControl(i);  
 }
 
-void Reverse() //Code to rotate the wheel Backward  
-{
-    motor('A', 'L', output);
-    motor('B', 'L', output);
-    Serial.print("R");
+void motorControl(int who) {
+  int speed = motorSpeed[who];
+  char dir = 'S';
+  if (speed > 0) dir = 'F';
+  if (speed < 0) {
+    dir = 'R';
+    speed = -speed;
+  }  
+  if (speed > motorCounter) {
+    motorControlDrive(who, dir);
+  }else {
+    motorControlDrive(who, 'S');
+  }
 }
+void motorControlDrive(int who, char dir){
+  int pin1 = motorPins[who][0];
+  int pin2 = motorPins[who][1];
 
-void Stop() //Code to stop both the wheels
-{
-    motor('A', 'L', 0);
-    motor('B', 'L', 0);
-    Serial.print("S");
+  if (dir == 'R') {
+    digitalWrite(pin1, 0);
+    digitalWrite(pin2, 1);    
+  }else if (dir == 'F') {
+    digitalWrite(pin1, 1);
+    digitalWrite(pin2, 0);    
+  } else {
+    digitalWrite(pin1, 1);
+    digitalWrite(pin2, 1);
+  }
 }
